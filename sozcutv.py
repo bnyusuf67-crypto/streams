b2ba21ca23592a8682c22252b606ec5d51a2fda7
bnyusuf67-crypto <bnyusuf67@gmail.com>
2026-08-21 16:41:39 +0300

    Update sozcutv.py

--- sozcutv.py
+++ sozcutv.py
@@ -4,7 +4,7 @@ import requests
 playlist_url = "https://raw.githubusercontent.com/k33n26/vavoo/refs/heads/main/iptv.m3u"
 output_file = "streams/sozcutv.m3u8"
 
-# Download playlist
+# Playlist'i indir
 response = requests.get(playlist_url)
 response.raise_for_status()
 
@@ -12,7 +12,7 @@ lines = response.text.splitlines()
 
 stream_url = None
 
-# Tırnak çakışmasını önlemek için dış tırnaklar tek tırnak (') yapıldı
+# Tırnak çakışmasını önlemek için dış tırnak tek tırnak
 target_line = '#EXTINF:-1 tvg-id="SozcuTV.tr@SD" tvg-name="SZC .c" tvg-logo="https://logo.huhu.to/logo?c=2036794972.png" group-title="Haber",SZC .c'
 
 for i, line in enumerate(lines):
@@ -24,13 +24,15 @@ for i, line in enumerate(lines):
 if not stream_url:
     raise Exception("SZC TV not found in playlist")
 
-# Download the referenced m3u8
-stream_response = requests.get(stream_url)
-stream_response.raise_for_status()
-
+# Klasör yoksa oluştur
 os.makedirs(os.path.dirname(output_file), exist_ok=True)
 
+# Şablona göre yeni Master M3U8 dosyasını oluştur
+m3u8_content = f"""#EXTM3U
+#EXT-X-STREAM-INF:BANDWIDTH=7680000
+{stream_url}"""
+
 with open(output_file, "w", encoding="utf-8") as f:
-    f.write(stream_response.text)
+    f.write(m3u8_content)
 
 print(f"Saved to {output_file}")
